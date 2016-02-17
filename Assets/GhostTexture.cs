6faacd323d087ed509df98fea6019ae537983e36
gamesketches <sam.von.ehren@gmail.com>
2016-02-16 23:07:16 -0500

    Created a script for scrolling a skull texture

---
+++ Assets/GhostTexture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostTexture : MonoBehaviour {
+
+	private Material[] baseTextures;
+	private Texture skullTexture;
+	private Renderer renderer;
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer>();
+		baseTextures = gameObject.GetComponent<Renderer>().materials;
+		skullTexture = Resources.Load("skulls") as Texture;
+		for(int i = 0; i < baseTextures.Length; i++) {
+			renderer.materials[i].mainTexture = skullTexture;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < baseTextures.Length; i++) {
+			renderer.materials[i].mainTextureOffset = new Vector2(Time.time * 0.90f, 0f);
+		}
+	}
+}
