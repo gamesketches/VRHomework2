98ef0250ca223a510ecb9972462ca73d4888eb58
gamesketches <sam.von.ehren@gmail.com>
2016-02-17 13:53:19 -0500

    Probably the final commit

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.VR;
+
+public class CharacterMovement : MonoBehaviour {
+
+	CharacterController controller;
+	public Camera mainCamera;
+	public GhostTexture hauntedWhiteBoard;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+
+
+		//VRSettings.renderScale = 0.7f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.R) ) { // let user recenter camera if they want to
+			InputTracking.Recenter();
+		}
+
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+		// move forward in the direction that the VR headset is looking
+		controller.Move( vertical * Camera.main.transform.forward * Time.deltaTime * 10f);
+		// turn left or right
+		transform.Rotate( 0f, horizontal * Time.deltaTime * 100f, 0f );
+		RaycastHit hit;
+		Ray PsychicRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+
+		if(Physics.Raycast(PsychicRay, out hit, 40f)){
+			if(hit.collider.tag == "whiteboard"){
+				Debug.Log("hit whiteboard");
+			}
+			else if(hit.collider.tag == "wall") {
+				hauntedWhiteBoard.hideSkulls();
+			}
+		}
+		Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.forward * 100f);
+	}
+}
--- Assets/GhostTexture.cs
+++ Assets/GhostTexture.cs
@@ -3,23 +3,44 @@ using System.Collections;
 
 public class GhostTexture : MonoBehaviour {
 
-	private Material[] baseTextures;
+	private Texture[] baseTextures;
 	private Texture skullTexture;
 	private Renderer renderer;
+	private int hideTimer;
 	// Use this for initialization
 	void Start () {
+		hideTimer = 100;
 		renderer = gameObject.GetComponent<Renderer>();
-		baseTextures = gameObject.GetComponent<Renderer>().materials;
+		baseTextures = new Texture[renderer.materials.Length];//gameObject.GetComponent<Renderer>().materials;
 		skullTexture = Resources.Load("skulls") as Texture;
 		for(int i = 0; i < baseTextures.Length; i++) {
+			baseTextures[i] = renderer.materials[i].mainTexture;
 			renderer.materials[i].mainTexture = skullTexture;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for(int i = 0; i < baseTextures.Length; i++) {
-			renderer.materials[i].mainTextureOffset = new Vector2(Time.time * 0.90f, 0f);
+		hideTimer--;
+		if(hideTimer == 0) {
+			for(int i = 0; i < baseTextures.Length; i++) {
+				renderer.materials[i].mainTexture = skullTexture;
+			}
 		}
+		else if(hideTimer < 0) {
+			for(int i = 0; i < baseTextures.Length; i++) {
+				renderer.materials[i].mainTextureOffset = new Vector2(Time.time * 0.30f, 0f);
+			}
+		}
+		else {
+			for(int i = 0; i < renderer.materials.Length; i++) {
+				renderer.materials[i].mainTexture = baseTextures[i];
+			}
+		}
+
+	}
+
+	public void hideSkulls() {
+		hideTimer = 100;
 	}
 }
